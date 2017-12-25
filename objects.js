9b5a109587bdc91f1b96d25f09d448f0223a9de9
Laura Luidolt <laura.luidolt@gmail.com>
2017-12-25 11:55:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {
   roam: 'Alive'
 };
 
-function updatePlaylist(playlist, artistName, songTitle){ 
+function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
 }
